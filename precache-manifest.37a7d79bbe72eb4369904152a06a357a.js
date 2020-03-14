ab3e5272321fb5f8431d1904bff7dbc5891c4072
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-03-14 13:28:38 +0300

    Updates

---
+++ precache-manifest.37a7d79bbe72eb4369904152a06a357a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8a478581396e8908bb90a23b47bdcc",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "34534199092b6fca44f7",
+    "url": "/counter/static/css/main.d4dcbb89.chunk.css"
+  },
+  {
+    "revision": "6aace0d87d2180a960d9",
+    "url": "/counter/static/js/2.585737d8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/counter/static/js/2.585737d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34534199092b6fca44f7",
+    "url": "/counter/static/js/main.aa4a3129.chunk.js"
+  },
+  {
+    "revision": "eb80d850577a167433f9",
+    "url": "/counter/static/js/runtime-main.1f6cc87f.js"
+  }
+]);
